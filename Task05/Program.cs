23664566a19ab4629952e95cd925f93d154013b3
Vitaliy <kv1993@me.com>
2023-05-19 12:04:44 +0300

    Added code to Task05 turn 1

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,15 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = -number;
+
+while( i <= number )
+{
+    Console.Write($"{i} ");
+    i++;
+}
