ce9dcba822adbd7efcc9e21e3b74c80f2c38464a
Vitaliy <kv1993@me.com>
2023-05-19 17:05:58 +0300

    Added code to Task07 turn 1

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая на вход принимает
+// трехзначное число и выводит последнюю цифру числа
+
+Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"{lastDigit} является последней цифрой числа {number}.");
+}   
+else
+{
+    Console.WriteLine("Неверное число.");
+}
