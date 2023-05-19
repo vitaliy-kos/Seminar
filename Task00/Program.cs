841698f0528ad50afd12c9b527fb5cc300b657eb
Vitaliy <kv1993@me.com>
2023-05-19 11:26:41 +0300

    Updated code to Task01 turn 1

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -9,5 +9,5 @@
 
 Console.Write("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int square = number*number;
+int square = number * number;
 Console.WriteLine($"Квадрат числа {number} равен {square}");
