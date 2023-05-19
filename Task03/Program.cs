1e3f162b0323d96b1c753aa33c7fadcdafa6b5f1
Vitaliy <kv1993@me.com>
2023-05-19 11:55:29 +0300

    Updated code to Task03 turn 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -8,34 +8,13 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (num >= 1 && num <= 7)
 {
-    if (num == 1)
-    {
-        Console.WriteLine("Понедельник");
-    }
-    else if (num == 2)
-    {
-        Console.WriteLine("Вторник");
-    }
-    else if (num == 3)
-    {
-        Console.WriteLine("Среда");
-    }
-    else if (num == 4)
-    {
-        Console.WriteLine("Четверг");
-    }
-    else if (num == 5)
-    {
-        Console.WriteLine("Пятница");
-    }
-    else if (num == 6)
-    {
-        Console.WriteLine("Суббота");
-    }
-    else if (num == 7)
-    {
-        Console.WriteLine("Воскресенье");
-    }
+    if (num == 1) Console.WriteLine("Понедельник");
+    else if (num == 2) Console.WriteLine("Вторник");
+    else if (num == 3) Console.WriteLine("Среда");
+    else if (num == 4) Console.WriteLine("Четверг");
+    else if (num == 5) Console.WriteLine("Пятница");
+    else if (num == 6) Console.WriteLine("Суббота");
+    else if (num == 7) Console.WriteLine("Воскресенье");
 }
 else
 {
