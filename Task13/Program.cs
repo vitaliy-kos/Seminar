565180a784cfcff7b50c5314e8ab94202f5642f1
Vitaliy <kv1993@me.com>
2023-05-23 13:26:28 +0300

    Added code Task013

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,9 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-срезаем в цикле по 1 цифре делением на 10
-условие цикла длина строки не меньше 3
-и остаток от деления на 10
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-двумя методами
-сначала получить 3 значное
-потом получить последнее число
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100)
+{
+    int thirdDigitNumber = GetThirdDigitNumber(number);
+    int thirdDigit = LastDigit(thirdDigitNumber);
+    Console.WriteLine($"Третья цифра = {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+
+int GetThirdDigitNumber(int num)
+{
+    while (num >= 1000) num = num / 10;
+    return num;
+}
+
+int LastDigit(int thirdDigitNumber)
+{
+    int thirdDigit = thirdDigitNumber % 10;
+    return thirdDigit;
+}
