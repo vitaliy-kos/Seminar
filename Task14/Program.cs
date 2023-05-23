029939569794785fb39396b77ecd6d87beb44a8f
Vitaliy <kv1993@me.com>
2023-05-23 12:02:39 +0300

    Added code Task014

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = DevidedByTwoNumbers(number, 7, 23);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool DevidedByTwoNumbers(int num, int num1, int num2)
+{
+    return num % num1 == 0 && num % num2 == 0;
+}
