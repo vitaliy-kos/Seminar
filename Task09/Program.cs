a5a615e81e97b0533f75376f17e3edb1213a73bf
Vitaliy <kv1993@me.com>
2023-05-23 10:11:31 +0300

    Added empty projects up to Task16

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
