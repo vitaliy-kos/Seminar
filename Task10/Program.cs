54b0ba43b17b79cc0e6b6692fc2e05f8953630a4
Vitaliy <kv1993@me.com>
2023-05-23 12:44:08 +0300

    Added code Task010

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number >= 100 && number <1000)
+{
+    int result = GetSecondDigit(number);
+    Console.WriteLine($"Итог -> {result}");
+}
+else 
+{
+    Console.WriteLine("Введено не трехзначное число.");
+}
+
+
+int GetSecondDigit(int num)
+{
+    int DevByTen = num / 10;
+    int res = DevByTen % 10;
+    return res;
+}
--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+две проверки 
+является ли от 1 до 7
+является ли выходным днем
