d94aa2722b49a69d7765b1b2c5359fa4b401e57f
Vitaliy <kv1993@me.com>
2023-05-23 11:37:01 +0300

    Added code Task012

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -4,4 +4,20 @@
 // второму, то программа выводит остаток от деления.
 // 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
-// 12 мин
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = Remain(num1, num2);
+string printLine = result == 0 ? "Кратно" : $"не кратно, остаток {result}";
+Console.WriteLine(printLine);
+
+int Remain(int number1, int number2)
+{
+    int res = number1 % number2;
+
+    return res;
+}
