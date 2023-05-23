55a3c53ca338c1b78cafabe4cb4a0d59c28baf7a
Vitaliy <kv1993@me.com>
2023-05-23 12:10:07 +0300

    Added code Task016

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -5,3 +5,17 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+Console.Write("Введите перове число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsSquareTwoNumbers(number1, number2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool IsSquareTwoNumbers(int num1, int num2)
+{
+    return num1*num1 == num2 || num2*num2 == num1;
+}
