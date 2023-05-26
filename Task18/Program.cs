fb39cc1657b5186538a17c6d302625587ba5eabb
Vitaliy <kv1993@me.com>
2023-05-26 11:14:06 +0300

    Updated code to Task18 turn 4

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,7 +3,7 @@
 // возможных координат точек в этой четверти (x и y).
 
 Console.Write("Введите номер четверти: ");
-int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
 string range = Range(quarter);
 string output = range == null 
@@ -14,9 +14,9 @@ Console.WriteLine(output);
 
 string Range(int q)
 {
-    if (q == 1) return "x > 0, y > 0";
-    if (q == 2) return "x < 0, y > 0";
-    if (q == 3) return "x < 0, y < 0";
-    if (q == 4) return "x > 0, y < 0";
+    if (q == "1") return "x > 0, y > 0";
+    if (q == "2") return "x < 0, y > 0";
+    if (q == "3") return "x < 0, y < 0";
+    if (q == "4") return "x > 0, y < 0";
     return null;
 }
