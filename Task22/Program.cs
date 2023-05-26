7efbc837a8da88bb768e69d525f3e61f453c096a
Vitaliy <kv1993@me.com>
2023-05-26 12:00:10 +0300

    Added code to Task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите наутральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+WriteLinesSquare(number);
+
+void WriteLinesSquare(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        int square = i * i;
+        Console.WriteLine($"{i} -> {square}");
+        i++;
+    }
+}
