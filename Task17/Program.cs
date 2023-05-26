e20d2b73a1741d5ce11b9672badaad4488b88d8a
Vitaliy <kv1993@me.com>
2023-05-26 10:31:34 +0300

    Added code to Task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+Console.WriteLine("Введите координаты точки!");
+
+Console.Write("X: ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y: ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xCoordinate, yCoordinate);
+string result = quarter > 0 
+                            ? $"Указанные координаты соответствуют четверти -> {quarter}" 
+                            : "Введены некорректные данные";
+
+Console.WriteLine(result);
+
+int Quarter(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
