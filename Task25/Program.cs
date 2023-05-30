5e55a692cb2fdd4c1240d9763095dc3ba41deea0
Vitaliy <kv1993@me.com>
2023-05-30 10:49:24 +0300

    Added code to Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите любое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = CountNum(number);
+Console.WriteLine($"Количество символов: {res}");
+
+int CountNum(int num) 
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        count ++;
+    }
+
+    return count;
+}
