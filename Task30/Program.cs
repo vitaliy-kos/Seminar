f981b7ffa5613e7914d17dd03d5ee16e7df03b91
Vitaliy <kv1993@me.com>
2023-05-30 12:00:26 +0300

    Added code to Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+
+int[] result = FillArray(array);
+
+PrintArray(result);
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+    Console.WriteLine("");
+}
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = CreateRandom();
+    }
+    return array;
+}
+
+int CreateRandom()
+{
+    return new Random().Next(2);
+}
