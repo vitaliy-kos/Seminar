4a2454ac94c2539d96073d417da7e7a6197cd884
Vitaliy <kv1993@me.com>
2023-05-30 11:15:06 +0300

    Added code to Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите любое натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0) 
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+
+int res = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number}: {res}");
+
+int Factorial(int num) 
+{
+    int result = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+}
