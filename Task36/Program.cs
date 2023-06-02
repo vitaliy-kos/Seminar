ee4eed6e51c3bd4676fdf2c6d013f56989b7563c
Vitaliy <kv1993@me.com>
2023-06-03 01:43:56 +0300

    Added code to Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,3 +1,45 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountSumOddPosition(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int arraySize = new Random().Next(5,7);
+int arrayMinVal = 1;
+int arrayMaxVal = 20;
+int[] array = CreateArrayRndInt(arraySize, arrayMinVal, arrayMaxVal);
+PrintArray(array);
+
+int sumOddPosition = CountSumOddPosition(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {sumOddPosition}");
