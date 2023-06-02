2333c9867dc540f0d80151f1f164e41131bbefdd
Vitaliy <kv1993@me.com>
2023-06-02 22:38:53 +0300

    Added code to Task35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -5,3 +5,43 @@
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountDigitInInterval(int[] arr, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= min && arr[i] <= max) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(123, -100, 100);
+PrintArray(array);
+
+int minInterval = 10;
+int maxInterval = 99;
+int countDigit = CountDigitInInterval(array, minInterval, maxInterval);
+Console.WriteLine($"Количество чисел в интервале от {minInterval} до {maxInterval} = {countDigit}");
