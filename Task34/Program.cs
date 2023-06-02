57167e8b8256e6940cf8286a60d03760786b05e9
Vitaliy <kv1993@me.com>
2023-06-02 22:25:13 +0300

    Added code to Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -3,3 +3,41 @@
 // чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(9, 1, 999);
+PrintArray(array);
+
+int countEven = CountEvenNumbers(array);
+Console.WriteLine($"Количество четных чисел = {countEven}");
+
