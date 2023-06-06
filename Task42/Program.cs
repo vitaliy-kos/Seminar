ad2168809441251e3229cce7f218c40c6c910950
Vitaliy <kv1993@me.com>
2023-06-06 11:17:46 +0300

    Added code to Task42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,3 +3,34 @@
 // 46 -> 101110
 // 13 -> 1101
 // 2 -> 10
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 0) return number;
+    else return -1;
+}
+
+int ConvertToBin(int num)
+{
+    int binNum = 0;
+    int mult = 1;
+    while (num > 0)
+    {
+        binNum = binNum + (num % 2 * mult); // 1 + (1 * 10) = 11
+        mult = mult * 10;
+        num = num / 2;
+    }
+    return binNum;
+}
+
+int number = GetNumber("Введите число: ");
+
+if (number == -1)
+{
+    Console.WriteLine("Число должно быть неотрицательным");
+    return; 
+}
+
+Console.WriteLine($"Число {number} в двоичном представлении -> {ConvertToBin(number)}");
