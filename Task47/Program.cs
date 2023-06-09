0031875e93ef555466b749487436644a311b0c1f
Vitaliy <kv1993@me.com>
2023-06-09 12:45:59 +0300

    Added code to Task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5      7    -2     -0,2
+//   1   -3,3     8     -9,9
+//   8    7,8  -7,1        9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Math.Round(rnd.NextDouble() * (max-min) + min, 1);
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 8} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2d = CreateMatrixRndDouble(3, 4, -10, 10);
+PrintMatrix(array2d);
