0925d4ef084d361e05aa2574bc707133fc0f0c94
Vitaliy <kv1993@me.com>
2023-06-19 18:03:03 +0300

    Updated code to Task55

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -7,3 +7,35 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1);
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 6} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2d);
--- Task55/Program.cs
+++ Task55/Program.cs
@@ -34,9 +34,9 @@ void PrintMatrix(int[,] matrix)
 
 void SwapRowToColumn(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < i; j++)
         {
             int temp = matrix[i, j];
             matrix[i, j] = matrix[j, i];
