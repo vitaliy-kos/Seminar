933ad149b5e1ee4a9e348695ff771ea4eb3673ef
Vitaliy <kv1993@me.com>
2023-06-20 02:47:21 +0300

    Added code to Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -10,7 +11,6 @@
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-
     int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -31,13 +31,32 @@ void PrintMatrix(int[,] matrix)
         Console.Write("|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i,j], 6} ");
+            Console.Write($"{matrix[i,j], 3} ");
         }
         Console.WriteLine("|");
+    }
+}
+
+void SortMatrix(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            for (var k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, j]) continue;
+                var temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
     }
 }
 
 int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
 PrintMatrix(array2d);
 
-Array.Sort(array2d);
+SortMatrix(array2d);
+Console.WriteLine("Sorted array:");
+PrintMatrix(array2d);
