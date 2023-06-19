1226f208f82b07a75c4d6b01f99f597892000c43
Vitaliy <kv1993@me.com>
2023-06-19 16:29:53 +0300

    Added code to Task53 and added task conditions in to other Tasks

--- Task53/Program.cs
+++ Task53/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max + 1);
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SwapFirstLastRow(int[,] matrix)
+{
+    int firstRow = 0;
+    int lastRow = matrix.GetLength(0) - 1;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int temp = matrix[firstRow, i];
+        matrix[firstRow, i] = matrix[lastRow, i];
+        matrix[lastRow, i] = temp;
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(array2d);
+
+SwapFirstLastRow(array2d);
+Console.WriteLine("Reverse first row and last row array:");
+PrintMatrix(array2d);
