0352c82d01cc8edb84edfa8c495c24c98813dd74
Vitaliy <kv1993@me.com>
2023-06-19 17:47:11 +0300

    Added code to Task55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -2,3 +2,64 @@
 // которая заменяет строки на столбцы. В случае, если это
 // невозможно, программа должна вывести сообщение для
 // пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SwapRowToColumn(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+bool IsMatrixSquare(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(array2d);
+
+if (IsMatrixSquare(array2d)) 
+{
+    SwapRowToColumn(array2d);
+    Console.WriteLine("Reverse row and column array:");
+    PrintMatrix(array2d);
+}
+else
+{
+    Console.WriteLine("Reverse row and column is impossible!");
+}
