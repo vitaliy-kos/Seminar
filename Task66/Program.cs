055e5a590487b917a8153f63b618ebc1c1254114
Vitaliy <kv1993@me.com>
2023-06-21 01:57:10 +0300

    Added code to Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -6,23 +6,29 @@
 
 int SumNumbersInRange(int num1, int num2)
 {
-    if (num1 < num2)
-    {
-        return SumNumbersInRange(num1 + 1, num2);
-    }
-    else if (num1 > num2)
-    {
-        return SumNumbersInRange(num1 - 1, num2);
-    }
-    else if (num1 == num2)
-    {
-        return num1;
-    }
-    return 1;
+    if (num1 == num2) return num1;
+    return num1 + SumNumbersInRange(num1 + 1, num2);
 }
 
-Console.WriteLine("Insert two natural numbers: ");
+Console.WriteLine("Insert first natural number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert second natural number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-int sum = SumNumbersInRange(number1, number2);
+if (number1 > 0 && number2 > 0)
+{
+    if (number1 < number2)
+    {
+        int sum = SumNumbersInRange(number1, number2);
+        Console.WriteLine($"The result is {sum}.");
+    }
+    else if (number1 > number2)
+    {
+        int sum = SumNumbersInRange(number2, number1);
+        Console.WriteLine($"The result is {sum}.");
+    }
+}
+else
+{
+    Console.WriteLine("Inserted number isn't natural!");
+}
