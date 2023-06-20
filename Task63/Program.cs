df87be89a7997ba8349f70efe61841692db47e24
Vitaliy <kv1993@me.com>
2023-06-21 00:40:16 +0300

    Added code to Task63

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+    NaturalNumbers(num-1);
+    Console.Write($"{num}, ");
+}
+
+NaturalNumbers(number);
+Console.WriteLine("");
