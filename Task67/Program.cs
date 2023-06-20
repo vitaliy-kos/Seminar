54c351f8fffacbffb9e30c7b007ea49852b660c3
Vitaliy <kv1993@me.com>
2023-06-21 01:06:56 +0300

    Added code to Task67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigitRec(int n)
+{
+    if (n == 0) return 0;
+    else return n % 10 + SumDigitRec(n / 10);
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumDigitRec(num));
