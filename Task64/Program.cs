30210e594ae14ba0f8af81d17f05d16b4ee7bbba
Vitaliy <kv1993@me.com>
2023-06-21 01:26:15 +0300

    Added code to Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,3 +3,21 @@
 // Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void ConsecutiveNumbersInRange(int num1)
+{
+    if (num1 > 1)
+    {
+        Console.Write($"{num1}, ");
+        ConsecutiveNumbersInRange(num1 - 1);
+    }
+    else
+    {
+        Console.WriteLine($"{num1}");
+    }
+}
+
+Console.WriteLine("Insert natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ConsecutiveNumbersInRange(number);
