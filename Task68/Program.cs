059d0c8103ade930d75dea1cbfcc1306ba754cc5
Vitaliy <kv1993@me.com>
2023-06-21 02:16:45 +0300

    Added code to Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -4,3 +4,26 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int AckermanCount(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    } 
+    else if (m == 0)
+    {
+        return AckermanCount(n - 1, 1);
+    }
+    else
+    {
+        return AckermanCount(n - 1, AckermanCount(n, m - 1));
+    }
+}
+
+Console.Write("Insert natural number n: ");
+int nNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert natural number m: ");
+int mNum = Convert.ToInt32(Console.ReadLine());
+
+int result = AckermanCount(nNum, mNum);
+Console.WriteLine($"The result is: {result}");
